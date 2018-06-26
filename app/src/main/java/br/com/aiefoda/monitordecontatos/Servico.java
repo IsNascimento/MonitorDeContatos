c5d4294576e31f45a2d006327c180dd3658304e8
AndersonPM <rhcpandersonph@gmail.com>
2018-06-26 13:32:01 -0300

    Adicionando as notificações

--- app/src/main/java/br/com/aiefoda/monitordecontatos/Servico.java
+++ app/src/main/java/br/com/aiefoda/monitordecontatos/Servico.java
@@ -2,15 +2,20 @@ package br.com.aiefoda.monitordecontatos;
 
 import android.app.Fragment;
 import android.app.LoaderManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.CursorLoader;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
+import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
 import android.provider.ContactsContract;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import java.util.ArrayList;
@@ -29,8 +34,81 @@ public class Servico extends Service{
     @Override
     public void onCreate(){
         super.onCreate();
+        createNotificationChannel();
+        preferencias = getSharedPreferences("preferencias", 0);
         Log.i("script","onCreate");
     }
+
+
+    /////////////////////////////////////////////////////////////
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Nome do canal";
+            String description = "Monitor de Contatos";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("canal", name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+
+            NotificationChannel canalU = new NotificationChannel(
+                    "canalUrgente", "Canal Urgente", NotificationManager.IMPORTANCE_HIGH);
+            canalU.setDescription("Canal de notificações urgentes");
+            notificationManager.createNotificationChannel(canalU);
+        }
+    }
+
+    public void criarNotificacao() {
+        Intent intent = new Intent(this, Servico.class);
+        PendingIntent intentPendente = PendingIntent.getActivity(this, 0, intent, 0);
+
+        NotificationCompat.Builder construtorDeNotificao =
+                new NotificationCompat.Builder(this, "canal")
+                        .setSmallIcon(R.drawable.ic_report_black_24dp)
+                        .setContentTitle("Monitor de Contatos")
+                        .setContentText("Número de contatos atingiu o limite")
+                        .setContentIntent(intentPendente)
+                        .addAction(R.drawable.ic_report_black_24dp, "IR", intentPendente)
+                        .setAutoCancel(true);
+
+        NotificationManager gerenciadorDeNotificacoes =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        gerenciadorDeNotificacoes.notify(1, construtorDeNotificao.build());
+    }
+
+    public void cancelarNotificacao() {
+        NotificationManager gerenciadorDeNotificacoes =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        gerenciadorDeNotificacoes.cancel(1);
+    }
+
+    public void atualizarNotificacao() {
+        Intent intent = new Intent(this, Servico.class);
+        PendingIntent intentPendente = PendingIntent.getActivity(this, 0, intent, 0);
+
+        NotificationCompat.Builder construtorDeNotificao =
+                new NotificationCompat.Builder(this, "canal")
+                        .setSmallIcon(R.drawable.ic_report_black_24dp)
+                        .setContentTitle("Notificação")
+                        .setContentText("Texto da notificação atualizado")
+                        .setStyle(new NotificationCompat.InboxStyle()
+                                .setBigContentTitle("Notificação Longa")
+                                .addLine("Linha 1")
+                                .addLine("Linha 2")
+                                .addLine("Linha 3")
+                                .addLine("Linha 4"))
+                        .setContentIntent(intentPendente)
+                        .addAction(R.drawable.ic_report_black_24dp, "IR", intentPendente)
+                        .setAutoCancel(true);
+
+        NotificationManager gerenciadorDeNotificacoes =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        gerenciadorDeNotificacoes.notify(1, construtorDeNotificao.build());
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////
+
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startID){
@@ -66,7 +144,7 @@ public class Servico extends Service{
             handler = new Handler(Looper.getMainLooper());
             while(ativo){
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(10000);
                     handler.post(new Runnable() {
                         @Override
                         public void run() {
@@ -74,6 +152,9 @@ public class Servico extends Service{
                             Cursor cursor = cursorLoader.loadInBackground();
                             int contatos = cursor.getCount();
                             Log.i("script","COUNT: "+contatos);
+                            if (contatos == preferencias.getInt("alerta", 50)){
+                                criarNotificacao();
+                            }
                         }
                     });
                 } catch (InterruptedException e) {
